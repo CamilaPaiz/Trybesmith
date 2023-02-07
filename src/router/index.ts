f6c0578a4fcbfe275f1a30a1be3d5d7cc15250f4
CamilaPaiz <camilapaiz@hotmail.com>
2023-02-07 16:54:20 -0300

    feat:validações para pessoas usuárias

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,19 +3,27 @@ import LoginController from '../controller/login.controller';
 import OrderController from '../controller/order.controller';
 import ProductControllers from '../controller/product.controllers';
 import UserController from '../controller/user.controller';
-import error from '../middlewares/error.middleware';
+import validate from '../middlewares/error.middleware';
+import validUser from '../middlewares/error.user.middleware';
 
 const router = Router();
 
 router.post(
   '/products', 
-  error.validateProductName,
-  error.validateProductAmount, 
+  validate.validateProductName,
+  validate.validateProductAmount, 
   ProductControllers.create,
 );
 router.get('/products', ProductControllers.getAll);
-router.post('/users', UserController.create);
+router.post(
+  '/users',
+  validUser.validateUsername,
+  validUser.validateLevel,
+  validUser.validateVocation,
+  validUser.validatePassword,
+  UserController.create,
+);
 router.get('/orders', OrderController.getAll);
-router.post('/login', error.validateLogin, LoginController.getLogin);
+router.post('/login', validate.validateLogin, LoginController.getLogin);
 
 export default router;
