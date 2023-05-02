bf6f877ad6188d623885c09e5108b6b63d82d619
CamilaPaiz <camilapaiz@hotmail.com>
2023-05-02 19:09:39 -0300

    fix:user.service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,6 @@
 import UserModel from '../models/user.model';
 import { IUser } from '../interfaces/User';
-/* import { userSchema } from '../validations/schema';
-import getError from '../errors/TypeErrorsUser'; */
- 
+
 async function create(
   username:string, 
   vocation:string, 
@@ -10,10 +8,6 @@ async function create(
   password:string,
 )
   :Promise<IUser> {
-  /*  const { error } = userSchema.validate(username, vocation, level, password);
-  if (error) {
-    return getError(error);
-  }  */
   const newUser = await UserModel.create(username, vocation, level, password);
   return newUser;
 }
